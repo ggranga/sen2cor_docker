810aabc593ad9ac74cc046e33a2fa5bbdb32de8b
ggranga <ggranga@gmail.com>
2017-08-01 16:40:53 +0200

    Do not use zip files but only unzipped SAFE

--- wrapper.sh
+++ wrapper.sh
@@ -5,14 +5,10 @@ source /root/sen2cor/L2A_Bashrc
 #set -x
 
 # Get input variables
-zipped_dir=/var/sentinel2_data/archives/"$1"
-unzipped_dir=/var/sentinel2_data/unzipped_scenes/"${1%.zip}"
+unzipped_dir=/var/sentinel2_data/unzipped_scenes/"$1"
 
 # Get the oupout resolution, if no resolution is set set the output resolution to 60
 outresolution="$2"
-
-# unzip the file 
-unzip -q $zipped_dir -d $unzipped_dir
 
 # Run sen2cor
 L2A_Process $unzipped_dir --resolution=$outresolution
--- wrapper_clean.sh
+++ wrapper_clean.sh
@@ -5,8 +5,7 @@ source /root/sen2cor/L2A_Bashrc
 #set -x
 
 # Get input variables
-zipped_dir=/var/sentinel2_data/archives/"$1"
-unzipped_dir=/var/sentinel2_data/unzipped_scenes/"${1%.zip}"
+unzipped_dir=/var/sentinel2_data/unzipped_scenes/"$1"
 
 # Delete the unzipped file after running sen2cor
 rm -r $unzipped_dir
